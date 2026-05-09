103c7613b34bc317967ebb4aded242eb0a020667
lihewu <2669011491@qq.com>
2026-05-09 14:00:34 +0800

    leetcode151反转字符串中单词 & StringBuffer训练

--- src/MyString.java
+++ src/MyString.java
@@ -1,21 +1,39 @@
 public class MyString {
     public static void main(String[] args) {
-
-        //实例化一个String(Stringy也是引用数据类型)
-        String str = "lihewu";//String 可以直接赋值，无需写new
-        System.out.println("str's length is " + str.length());
-
-        //通过new来创建Stirng
-        String str2 = new String("new lihewu");
-        for(char i : str.toCharArray()){
-            System.out.print(i );
+        String sentence = "Java,Python,C++";
+        String[] languages = sentence.split(",");
+        for(String str : languages){
+            System.out.println(str);
         }
-        System.out.println();
 
-        //charAt();
-        System.out.println(str.charAt(2));
+        String str = "hello lihewu  !";
+        // String[] s = str.split(" ");//只能划分单个分隔符
+        String[] s = str.split("\\s+");
+        for(String s1 : s){
+            System.out.println(s1);
+        }
 
-        //toUpperCase()
-        System.out.println(str.toUpperCase());
     }
+
+
+
+//    public static void main(String[] args) {
+//
+//        //实例化一个String(Stringy也是引用数据类型)
+//        String str = "lihewu";//String 可以直接赋值，无需写new
+//        System.out.println("str's length is " + str.length());
+//
+//        //通过new来创建Stirng
+//        String str2 = new String("new lihewu");
+//        for(char i : str.toCharArray()){
+//            System.out.print(i );
+//        }
+//        System.out.println();
+//
+//        //charAt();
+//        System.out.println(str.charAt(2));
+//
+//        //toUpperCase()
+//        System.out.println(str.toUpperCase());
+//    }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,28 +1,98 @@
-import java.util.Map;
-import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
 
+/*
+LeetCode.151
+反转字符串中的单词顺序
+ */
 class Solution {
-    public boolean canConstruct(String ransomNote, String magazine) {
-        if(ransomNote.length() > magazine.length()){//如果所需字符串>给定字符串，直接return false;
-            return false;
+    public String reverseWords(String s) {
+        //借助split()，非O(1)
+        String[] str = s.trim().split("\\s+");//分割字符
+        StringBuffer sb = new StringBuffer();
+        //String ret = new String();
+        for(int i = str.length -1; i > 0; i--){
+            sb.append(str[i]);
+            //性能杀手
+            //ret = ret + str[i]+" ";//Java当中String类型具有不可变性，使用+实际上是新建了一个String对象并进行赋值
         }
-        char[] count = new char[26];//记录ransomNote所需的字符数
-        for(char c : ransomNote.toCharArray()){
-            count[c - 'a']++;
+        if(!str[0].equals(" ")){
+            sb.append(str[0]);
         }
-
-        for(char c : magazine.toCharArray()){
-            count[c -'a']--;
-        }
-
-        for(int i : count){
-            if(i < 0){
-                return false;
-            }
-        }
-        return true;
+        return sb.toString();
     }
 }
+//class Solution {
+//    public String reverseWords(String s) {
+//        //char[] ch = s.toCharArray();
+//        String sentence = "Java,Python,C++";
+//        String[] languages = sentence.split(",");
+//
+//    }
+//}
+
+
+
+//class Solution {
+//    public List<List<Integer>> threeSum(int[] nums) {
+//        List<List<Integer>> ret = new ArrayList<>();
+//        Arrays.sort(nums);//对nums进行排序
+//        // a = nums[i], b = nums[left], c = nums[right]
+//        for(int i = 0; i < nums.length; i++){
+//            if(nums[i] > 0){//如果第一个元素的值>0,则后续不可能凑出三元组
+//                return ret;
+//            }
+//            if(i > 0 && nums[i] == nums[i-1]){//当出现连续的字符时，需要去重
+//                continue;
+//            }
+//            int left = i+1;
+//            int right = nums.length-1;
+//            while(left < right){//定一个，动两个
+//                int sum = nums[i]+nums[left]+nums[right];
+//                if(sum > 0){
+//                    right--;//如果sum值过大，则需--
+//                }
+//                if(sum < 0){
+//                    left++;
+//                }
+//                if(sum == 0){//如果符合条件，则同样需要去重
+//                    ret.add(Arrays.asList(nums[i],nums[left],nums[right]));
+//                    while(left < right && nums[left] == nums[left-1]) left++;
+//                    while(left < right && nums[right] == nums[right+1]) right--;
+//                }
+//            }
+//        }
+//        return ret;
+//
+//    }
+//}
+
+//import java.util.Map;
+//import java.util.HashMap;
+//
+//class Solution {
+//    public boolean canConstruct(String ransomNote, String magazine) {
+//        if(ransomNote.length() > magazine.length()){//如果所需字符串>给定字符串，直接return false;
+//            return false;
+//        }
+//        char[] count = new char[26];//记录ransomNote所需的字符数
+//        for(char c : ransomNote.toCharArray()){
+//            count[c - 'a']++;
+//        }
+//
+//        for(char c : magazine.toCharArray()){
+//            count[c -'a']--;
+//        }
+//
+//        for(int i : count){
+//            if(i < 0){
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
+//}
 
 //import java.util.Map;
 //import java.util.HashMap;
